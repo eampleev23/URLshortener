0a65d54ea930e9342133ae2abd10859ccc28b0d3
Евгений Амплеев <ampleeve@192.168.1.4>
2023-10-30 20:31:11 +0300

    pattern mainpage

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,7 +2,13 @@ package main
 
 import "net/http"
 
+func mainPage(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte("Привет!"))
+}
+
 func main() {
+	http.HandleFunc(`/`, mainPage)
+
 	err := http.ListenAndServe(`:8080`, nil)
 	if err != nil {
 		panic(err)
