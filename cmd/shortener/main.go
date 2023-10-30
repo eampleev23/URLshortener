2128408ce751884561e4bb14533788801f3fcf57
Евгений Амплеев <ampleeve@192.168.1.4>
2023-10-30 21:02:28 +0300

    endpoint postShortLink

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,12 +2,28 @@ package main
 
 import "net/http"
 
-func mainPage(res http.ResponseWriter, req *http.Request) {
-	res.Write([]byte("Привет!"))
+func postShortLink(res http.ResponseWriter, req *http.Request) {
+
+	if req.Method == http.MethodPost {
+
+		// установить статус 201
+		res.WriteHeader(201)
+
+		// установить тип контента text/plain
+		res.Header().Set("content-type", "text/plain")
+
+		// добавить в качестве ответа сокращенный урл
+		res.Write([]byte("http://localhost:8080/EwHXdJfB"))
+
+	} else {
+		// установить статус 201
+		res.WriteHeader(http.StatusMethodNotAllowed)
+	}
+
 }
 
 func main() {
-	http.HandleFunc(`/`, mainPage)
+	http.HandleFunc(`/`, postShortLink)
 
 	err := http.ListenAndServe(`:8080`, nil)
 	if err != nil {
