c2f86b91c58050a52f7656c3f6995235a793946f
Евгений Амплеев <ampleeve@192.168.1.5>
2023-11-25 14:44:24 +0300

    iter6 final

--- cmd/shortener/handlers.go
+++ cmd/shortener/handlers.go
@@ -8,40 +8,43 @@ import (
 )
 
 func createShortLink(res http.ResponseWriter, req *http.Request) {
-	// Сначала необходимо убедиться, что запрос корректный (в теле должна быть строка как text/plain в виде урл
-	// а вернуть должен код 201 и сокращенный урл как text/plain
-	// заводим строковую переменную чтобы хранить в ней значение длинной ссылки
-	var longLink string
-	if b, err := io.ReadAll(req.Body); err == nil {
-		longLink = string(b)
+
+	if req.Method == http.MethodPost {
+		// Сначала необходимо убедиться, что запрос корректный (в теле должна быть строка как text/plain в виде урл
+		// а вернуть должен код 201 и сокращенный урл как text/plain
+		// заводим строковую переменную чтобы хранить в ней значение длинной ссылки
+		var longLink string
+		if b, err := io.ReadAll(req.Body); err == nil {
+			longLink = string(b)
+		}
+		// Генерируем короткую ссылку для переданной длинной
+		shortLink, err := generateUniqShortLink()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Записываем в бд // В качестве индекса мапы используем короткую ссылку чтобы можно было быстро найти
+		linksCouples[shortLink] = longLink
+
+		// Устанавливаем статус 201
+		res.WriteHeader(201)
+
+		// Устаннавливаем тип контента text/plain
+		res.Header().Set("content-type", "text/plain")
+
+		// В качестве ответа возвращаем сокращенный урл с именем домена
+		//shortLinkWithPrefix := "http://localhost:8080/" + shortLink
+		shortLinkWithPrefix := baseShortURL + "/" + shortLink
+		res.Write([]byte(shortLinkWithPrefix))
+	} else {
+		res.WriteHeader(http.StatusBadRequest)
 	}
-	// Генерируем короткую ссылку для переданной длинной
-	shortLink, err := generateUniqShortLink()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Записываем в бд // В качестве индекса мапы используем короткую ссылку чтобы можно было быстро найти
-	linksCouples[shortLink] = longLink
-
-	// Устанавливаем статус 201
-	res.WriteHeader(201)
-
-	// Устаннавливаем тип контента text/plain
-	res.Header().Set("content-type", "text/plain")
-
-	// В качестве ответа возвращаем сокращенный урл с именем домена
-	//shortLinkWithPrefix := "http://localhost:8080/" + shortLink
-	shortLinkWithPrefix := baseShortURL + "/" + shortLink
-	res.Write([]byte(shortLinkWithPrefix))
 
 }
 
 func useShortLink(res http.ResponseWriter, req *http.Request) {
 
-	if req.Method != http.MethodGet {
-		res.WriteHeader(http.StatusBadRequest)
-	} else {
+	if req.Method == http.MethodGet {
 		res.Header().Add("Location", linksCouples[chi.URLParam(req, "id")])
 		// добавляю для эксперимента
 		res.WriteHeader(http.StatusTemporaryRedirect)
@@ -57,7 +60,8 @@ func useShortLink(res http.ResponseWriter, req *http.Request) {
 		} else { // иначе это успех, есть совпадение, ставим 307 и в заголовок ответа локейшн отправляем длинную ссылку
 			res.WriteHeader(http.StatusTemporaryRedirect)
 		}
-
+	} else {
+		res.WriteHeader(http.StatusBadRequest)
 	}
 
 }
--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -32,16 +32,57 @@ func Initialize(level string) error {
 	return nil
 }
 
+type (
+	// берём структуру для хранения сведений об ответе
+	responseData struct {
+		status int
+		size   int
+	}
+
+	// добавляем реализацию http.ResponseWriter
+	loggingResponseWriter struct {
+		http.ResponseWriter // встраиваем оригинальный http.ResponseWriter
+		responseData        *responseData
+	}
+)
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	// записываем ответ, используя оригинальный http.ResponseWriter
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size // захватываем размер
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	// записываем код статуса, используя оригинальный http.ResponseWriter
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode // захватываем код статуса
+}
+
 // RequestLogger — middleware-логер для входящих HTTP-запросов.
 func RequestLogger(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		next.ServeHTTP(w, r)
+
+		responseData := &responseData{
+			status: 0,
+			size:   0,
+		}
+
+		lw := loggingResponseWriter{
+			ResponseWriter: w, // встраиваем оригинальный http.ResponseWriter
+			responseData:   responseData,
+		}
+
+		next.ServeHTTP(&lw, r)
+
 		duration := time.Since(start)
 		Log.Info("got incoming HTTP request",
-			zap.String("method", r.Method),
 			zap.String("path", r.URL.Path),
+			zap.String("method", r.Method),
 			zap.String("duration", shortDur(duration)),
+			zap.Int("status", responseData.status),
+			zap.Int("size", responseData.size),
 		)
 	}
 	return http.HandlerFunc(fn)
