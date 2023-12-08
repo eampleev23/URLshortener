bb8f7d71c7a77848753be013d45456ab232d843a
Евгений Амплеев <ampleeve@192.168.1.5>
2023-12-08 18:48:56 +0300

    models.go all

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,11 +1,11 @@
 package models
 
-// Request описывает запрос пользователя на получение короткой ссылки
+// RequestAddShortURL описывает запрос пользователя на получение короткой ссылки.
 type RequestAddShortURL struct {
 	LongURL string `json:"url"`
 }
 
-// Request описывает запрос пользователя на получение короткой ссылки
+// ResponseAddShortURL описывает запрос пользователя на получение короткой ссылки.
 type ResponseAddShortURL struct {
 	ShortURL string `json:"result"`
 }
