79aab8bdd8d1a7b0aa7a974bafb4e84c3a49d054
Евгений Амплеев <ampleeve@MacBook-Pro-Evgenij.local>
2024-01-16 08:05:28 +0300

    refactoring handler

--- internal/store/db_store.go
+++ internal/store/db_store.go
@@ -6,7 +6,6 @@ import (
 	"embed"
 	"errors"
 	"fmt"
-	"log"
 	"net/url"
 	"time"
 
@@ -190,64 +189,31 @@ func (ds DBStore) GetURLsByOwnerID(ctx context.Context, ownerID int) ([]LinksCou
 }
 
 func updateLinksCouplesStatement(count int, req []string, ownerID int) string {
-	valueParts1 := ""
-
+	valueParts := ""
 	for i := 0; i < count; i++ {
 		if i == count-1 {
-			valueParts1 = valueParts1 + fmt.Sprintf("('%s', %t, %d)", req[i], true, ownerID)
+			valueParts = valueParts + fmt.Sprintf("('%s', %t, %d)", req[i], true, ownerID)
 		} else {
-			valueParts1 = valueParts1 + fmt.Sprintf("('%s', %t, %d), ", req[i], true, ownerID)
+			valueParts = valueParts + fmt.Sprintf("('%s', %t, %d), ", req[i], true, ownerID)
 		}
 	}
-	//const stmtTmpl = `update links_couples set is_deleted=tmp.is_deleted::boolean from (values %s) as tmp (short_url, is_deleted, owner_id) where links_couples.short_url=tmp.short_url and links_couples.owner_id::int=tmp.owner_id::int;`
-	stmtTmpl := `update links_couples set is_deleted = tmp.is_deleted from (values ` + valueParts1 + `) as tmp (short_url, is_deleted, owner_id) where links_couples.short_url=tmp.short_url and links_couples.owner_id=tmp.owner_id;`
-	//valuesParts := make([]string, 0, count)
-	//numColumns := 3
-	//for i := 0; i < count; i++ {
-	//
-	//	valuesParts = append(
-	//		valuesParts,
-	//		fmt.Sprintf(
-	//			"($%d, $%d, $%d)",
-	//			i*numColumns+1, i*numColumns+2, i*numColumns+3, //nolint:gomnd // not magik
-	//		),
-	//	)
-	//
-	//}
-	//result := strings.Join(valuesParts, ",")
-	//log.Println("result=", result)
-	//return fmt.Sprintf(stmtTmpl, strings.Join(valuesParts, ","))
-	return stmtTmpl
-}
-func updateLinksCouplesFields(count int, req []string, ownerID int) []any {
-	values := make([]any, 0, 3*count) //nolint:gomnd // not magik
-	for i := 0; i < count; i++ {
-		values = append(
-			values,
-			req[i],
-			bool(true),
-			ownerID,
-		)
-	}
-	return values
+	stmtResult := `update links_couples set is_deleted
+  				  = tmp.is_deleted from (values ` + valueParts +
+		`) as tmp (short_url, is_deleted, owner_id) where links_couples.short_url=tmp.short_url
+					and links_couples.owner_id=tmp.owner_id;`
+	return stmtResult
 }
 
+// DeleteURLS проставляет тем урлам флаг удаления, которые пользователь решает удалить
 func (ds DBStore) DeleteURLS(ctx context.Context, ownerID int, req []string) (err error) {
-	// пока просто переберем все элементы, убедимся что они пришли
-	for i, v := range req {
-		log.Println("i=", i, "v=", v)
-	}
-	log.Println("DeleteURLS got ownerID=", ownerID)
-	// пришли. Значит разбираемся с батчингом
-	// у нас есть строковый массив req, нам нужно по каждому элементу изменить статус удаления на тру если запрос
-	// пришел от собственника урла
-
 	// Запускаем транзакцию
 	tx, err := ds.dbConn.BeginTx(ctx, nil)
+
 	// Обрабатываем ошибку
 	if err != nil {
 		return fmt.Errorf("failed to start a transaction: %w", err)
 	}
+
 	// Отложенно откатываем транзакцию если err != nil
 	defer func() {
 		if err := tx.Rollback(); err != nil {
@@ -256,14 +222,12 @@ func (ds DBStore) DeleteURLS(ctx context.Context, ownerID int, req []string) (er
 			}
 		}
 	}()
+
 	// задаем максимальное количество запросов за один раз
 	batchSize := len(req)
-	// подготавливаем выражение (оно возвращается без подставленных значеницй)
-	stmt := updateLinksCouplesStatement(batchSize, req, ownerID)
-	log.Println("stmt=", stmt)
-	//fields := updateLinksCouplesFields(batchSize, req, ownerID)
-	//log.Println("fields=", fields)
 
+	// подготавливаем запрос
+	stmt := updateLinksCouplesStatement(batchSize, req, ownerID)
 	if _, err := tx.Exec(stmt); err != nil {
 		return fmt.Errorf("failed to update a batch with: %w", err)
 	}
