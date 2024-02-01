6da755c32eac506f1b3d52eed3adb07f1543b291
Евгений Амплеев <ampleeve@192.168.1.5>
2024-02-01 07:55:45 +0300

    delete else

--- internal/handlers/get_URLs_by_userID.go
+++ internal/handlers/get_URLs_by_userID.go
@@ -18,33 +18,33 @@ func (h *Handlers) GetURLsByUserID(w http.ResponseWriter, r *http.Request) {
 		// Значит это первый запрос пользователя (куку установили и у нас есть ид, но статус надо отдать не авторизован).
 		w.WriteHeader(http.StatusUnauthorized)
 		return
-	} else {
-		// Значит пользователь авторизован, надо получить id из куки
-		cookie, _ := r.Cookie("token")
-		userID, _ := h.au.GetUserID(cookie.Value)
-		h.l.ZL.Debug("User id получили из куки (не из контекста)", zap.Int("userID", userID))
-
-		ownersURLs, err := h.s.GetURLsByOwnerID(r.Context(), userID)
-		if err != nil {
-			h.l.ZL.Debug("Error GetURLsByOwnerID:", zap.Error(err))
-		}
-		if len(ownersURLs) == 0 {
-			h.l.ZL.Debug("User id когда попали в условие что 0 контента", zap.Int("userID", userID))
-			w.WriteHeader(http.StatusNoContent)
-			return
-		}
-		enc := json.NewEncoder(w)
-		w.Header().Set("content-type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		ownersURLsD, err := models.GetResponseGetOwnerURLs(ownersURLs)
-		if err != nil {
-			h.l.ZL.Info("GetResponseGetOwnerURLs error", zap.Error(err))
-			return
-		}
-		if err := enc.Encode(ownersURLsD); err != nil {
-			h.l.ZL.Info("error encoding response in handler", zap.Error(err))
-			w.WriteHeader(http.StatusUnprocessableEntity)
-			return
-		}
 	}
+	// Значит пользователь авторизован, надо получить id из куки
+	cookie, _ := r.Cookie("token")
+	userID, _ := h.au.GetUserID(cookie.Value)
+	h.l.ZL.Debug("User id получили из куки (не из контекста)", zap.Int("userID", userID))
+
+	ownersURLs, err := h.s.GetURLsByOwnerID(r.Context(), userID)
+	if err != nil {
+		h.l.ZL.Debug("Error GetURLsByOwnerID:", zap.Error(err))
+	}
+	if len(ownersURLs) == 0 {
+		h.l.ZL.Debug("User id когда попали в условие что 0 контента", zap.Int("userID", userID))
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	enc := json.NewEncoder(w)
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	ownersURLsD, err := models.GetResponseGetOwnerURLs(ownersURLs)
+	if err != nil {
+		h.l.ZL.Info("GetResponseGetOwnerURLs error", zap.Error(err))
+		return
+	}
+	if err := enc.Encode(ownersURLsD); err != nil {
+		h.l.ZL.Info("error encoding response in handler", zap.Error(err))
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		return
+	}
+
 }
