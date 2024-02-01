1b2ab22a870992440e5697922f81468a6443f6f4
Евгений Амплеев <ampleeve@192.168.1.5>
2024-02-01 08:15:01 +0300

    fix handler1 to handler

--- internal/handlers/json_handler.go
+++ internal/handlers/json_handler.go
@@ -2,56 +2,70 @@ package handlers
 
 import (
 	"encoding/json"
-	"errors"
+	"fmt"
 	"net/http"
+	"net/url"
 
 	"github.com/eampleev23/URLshortener/internal/models"
-	"github.com/eampleev23/URLshortener/internal/store"
 	"go.uber.org/zap"
 )
 
 func (h *Handlers) JSONHandler(w http.ResponseWriter, r *http.Request) {
-	// Создаем модель для парсинга в нее запроса
-	var req models.RequestAddShortURL
-	dec := json.NewDecoder(r.Body)
-	if err := dec.Decode(&req); err != nil {
-		h.l.ZL.Info("cannot decode request JSON body", zap.Error(err))
-		w.WriteHeader(http.StatusBadRequest)
+	// Получаем id пользователя.
+	userID, err := h.GetUserID(r)
+	if err != nil {
+		h.l.ZL.Debug("Error getting userID", zap.Error(err))
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	defaultValue := 12
-	shortURL, err := h.s.SetShortURL(r.Context(), req.OriginalURL, defaultValue)
+	h.l.ZL.Debug("Got userID", zap.Int("userID", userID))
+
+	reqModel, err := getModelRequest(r)
 	if err != nil {
-		h.l.ZL.Debug("Ошибка создания новой ссылки: ", zap.Error(err))
-		// Если такая ссылка уже есть в базе, возвращаем шорт для нее
-		if errors.Is(err, store.ErrConflict) {
-			// пытаемся получить ссылку для оригинального урл, который уже есть в базе
-			shortURL, err = h.s.GetShortURLByOriginal(r.Context(), req.OriginalURL)
-			if err != nil {
-				h.l.ZL.Error("ошибка получения существующей короткой ссылки при конфликте", zap.Error(err))
-			}
-			shortLinkWithPrefix := h.c.BaseShortURL + "/" + shortURL
-			resp := models.ResponseAddShortURL{ShortURL: shortLinkWithPrefix}
-			enc := json.NewEncoder(w)
-			w.Header().Set("content-type", "application/json")
-			w.WriteHeader(http.StatusConflict)
-			if err := enc.Encode(resp); err != nil {
-				h.l.ZL.Info("encoding response error", zap.Error(err))
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
+		h.l.ZL.Info("getModelRequest error", zap.Error(err))
+	}
+	shortURL, err := h.s.SetShortURL(r.Context(), reqModel.OriginalURL, userID)
+	if err != nil {
+		h.l.ZL.Info("SetShortURL error", zap.Error(err))
+		shortURL, err = returnShortURLIfConflict(h, r, reqModel.OriginalURL, err)
+		if err != nil {
+			h.l.ZL.Info("returnShortURLIfConflict error", zap.Error(err))
+			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
+		w.Header().Set("content-type", "application/json")
+		w.WriteHeader(http.StatusConflict)
+		resp := models.ResponseAddShortURL{ShortURL: shortURL}
+		enc := json.NewEncoder(w)
+		if err := enc.Encode(resp); err != nil {
+			h.l.ZL.Info("error encoding response", zap.Error(err))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		return
 	}
-	// Здесь получается такой ссылки нет в базе, создаем новую
-	shortLinkWithPrefix := h.c.BaseShortURL + "/" + shortURL
-	resp := models.ResponseAddShortURL{ShortURL: shortLinkWithPrefix}
+	shortURL, err = url.JoinPath(h.c.BaseShortURL, shortURL)
+	if err != nil {
+		h.l.ZL.Info("url.JoinPath error", zap.Error(err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	resp := models.ResponseAddShortURL{ShortURL: shortURL}
 	enc := json.NewEncoder(w)
 	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(http.StatusCreated)
-	if err := enc.Encode(resp); err != nil {
+	err = enc.Encode(resp)
+	if err != nil {
 		h.l.ZL.Info("error encoding response", zap.Error(err))
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+}
+
+func getModelRequest(r *http.Request) (reqModel models.RequestAddShortURL, err error) {
+	dec := json.NewDecoder(r.Body)
+	if err := dec.Decode(&reqModel); err != nil {
+		return models.RequestAddShortURL{}, fmt.Errorf("dec.Decode error: %w", err)
+	}
+	return reqModel, nil
 }
