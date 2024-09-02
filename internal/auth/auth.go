bcf89ed29482b6f0979e76af9531f1fa036192b7
System Administrator <root@192.168.1.2>
2024-09-02 12:50:45 +0300

    iter18 fix auth

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -36,10 +36,10 @@ func Initialize(secretKey string, tokenExp time.Duration, l *logger.ZapLog) (*Au
 // при первом запросе неавторизованного пользователя(насолько я помню).
 type Key string
 
-const (
-	KeyUserIDCtx Key = "user_id_ctx"
-)
+// KeyUserIDCtx определяем название ключа через константу для того чтобы исключить конфликта данных
+const KeyUserIDCtx Key = "user_id_ctx"
 
+// Auth - метод авторизатора, подключаемый в роутинге как миддлвар
 func (au *Authorizer) Auth(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		_, err := r.Cookie("token")
@@ -68,6 +68,8 @@ func (au *Authorizer) Auth(next http.Handler) http.Handler {
 	}
 	return http.HandlerFunc(fn)
 }
+
+// setNewCookie - метод авторизатора, который устанавливает новую куку неавторизованному пользователю
 func (au *Authorizer) setNewCookie(w http.ResponseWriter) (int, error) {
 	// Генерируем случайный ид пользователя.
 	maxID := 10000
